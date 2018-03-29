c10756238e70a574656425cb3034086dfd456ae4
Jan-Erik Rediger <janerik@fnordig.de>
2018-03-29 09:54:06 +0200

    Deref to a byte slice in order to read and write from the page

--- tests/pager.rs
+++ tests/pager.rs
@@ -1,6 +1,6 @@
 extern crate memory_pager;
 
-use memory_pager::{Page, Pager};
+use memory_pager::Pager;
 
 #[test]
 fn can_create_default() {
@@ -17,12 +17,12 @@ fn can_get() {
   let mut pager = Pager::default();
   {
     let page = pager.get(0);
-    assert_eq!(page.buffer.len(), 1024);
+    assert_eq!(page.len(), 1024);
   }
 
   {
     let page = pager.get(3);
-    assert_eq!(page.buffer.len(), 1024);
+    assert_eq!(page.len(), 1024);
   }
 }
 
@@ -31,11 +31,21 @@ fn can_alloc() {
   let mut pager = Pager::default();
   {
     let page = &pager.get(16);
-    assert_eq!(page.buffer.len(), 1024);
+    assert_eq!(page.len(), 1024);
   }
 
   {
     let page = &pager.get(32);
-    assert_eq!(page.buffer.len(), 1024);
+    assert_eq!(page.len(), 1024);
   }
+}
+
+#[test]
+fn can_write() {
+  let mut pager = Pager::default();
+  let page = pager.get_mut(0);
+  page[0] = 1;
+
+  assert_eq!(1, page[0]);
+  assert_eq!(0, page[1]);
 }
